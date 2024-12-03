17fca682bdcebee5fc3c6c87921d48f90b4b4a04
rabiyarumi <rabiyaarumi@gmail.com>
2024-12-04 04:07:55 +0600

    added navbar & footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
-        <div>
-            Home here
+        <div className='flex flex-col min-h-screen'>
+           <Navbar/>
+           <div className='flex-grow'>
+            <Outlet/>
+           </div>
+           <Footer/>
         </div>
     );
 };
